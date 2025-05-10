d44ac752f83cdd783ee58c5c9ef16ceaaaf87830
AryanMir15 <aryanmir115@gmail.com>
2025-05-10 18:25:03 +0500

    Hoping

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 require("express-async-errors");
 
 // Import security packages
-const helmet = require("helmet");
-const cors = require("cors");
+// const helmet = require("helmet");
+// const cors = require("cors");
 
 const serverless = require("serverless-http");
 const connectDB = require('../db/connect');
@@ -13,8 +13,8 @@ const app = require("../app");
 connectDB(process.env.MONGO_URI);
 
 // Add any security middleware (optional)
-app.use(helmet());
-app.use(cors());
+// app.use(helmet());
+// app.use(cors());
 
 // Export the app as a serverless function for Vercel
 module.exports.handler = serverless(app);
